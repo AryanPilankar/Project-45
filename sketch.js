514e7313fe70d201bf8882c3e1a2b8f1a82a7c2e
AryanPilankar <82805828+AryanPilankar@users.noreply.github.com>
2022-03-26 12:44:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+
+function preload() {
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+   
+}
+
+function draw(){
+    
+        background(backgroundImg);
+    
+        
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+       
+}
+
